7b3877f4e2dad89ba2d1e424afecd34d99f6236c
outbounder <obiwonn@gmail.com>
2016-01-21 19:00:22 +0200

    fix: nginx json require to be relative at cwd
    + add missing format / string-template dep

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = function (angel) {
   require('angelabilities-exec')(angel)
   angel.on('nginx update :nginxCellPath', function (angel) {
+    var path = require('path')
     var nginxCellPath = angel.cmdData.nginxCellPath
-    var nginxCell = require(nginxCellPath)
+    var nginxCell = require(path.join(process.cwd(), nginxCellPath))
+    var format = require('string-template')
     var cmd = format("scp {local} {remote}:{dest} && ssh {remote} '{restartCmd}'", nginxCell)
     console.info(cmd)
     var child = angel.sh(cmd, function (err) {
@@ -13,5 +15,8 @@ module.exports = function (angel) {
         process.exit(0)
       }
     })
+    process.stdin.resume()
+    process.stdin.pipe(child.stdin)
   })
+  .example('nginx update ./dna/_staging/nginx.json')
 }
